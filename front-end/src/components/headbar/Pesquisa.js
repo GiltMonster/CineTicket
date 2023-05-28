325fac9fddd906b04e42fe5b85b5cfd2adba1485
Rodrigo Ferreira Pereira <45149332+RudrigoPereira@users.noreply.github.com>
2023-05-28 02:16:32 -0300

    caixa de pesquisa funcionando com a tecla enter

--- front-end/src/components/headbar/Pesquisa.js
+++ front-end/src/components/headbar/Pesquisa.js
@@ -1,19 +1,36 @@
-import { useState} from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 import "../../style/Header.css";
 
 export default function PesquisaFilmes() {
+  const [pesquisa, setPesquisa] = useState("");
+  const navigate = useNavigate();
 
-    const [pesquisa, setPesquisa] = useState()
-    
-    
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      navigate(`/pesquisarFilme/${pesquisa}`);
+      window.location.reload();
+    }
+  };
 
-    return (
-        <>
-            <Link reloadDocument className="search-icon" to={`/pesquisarFilme/${pesquisa}`}><FaSearch/></Link>
-            <input type="text" id="pesquisa" placeholder="Pesquisar:" onChange={(e) => setPesquisa(e.target.value)}/>
-        </>
-
-    )
+  return (
+    <>
+      <Link
+        reloadDocument
+        className="search-icon"
+        to={`/pesquisarFilme/${pesquisa}`}
+      >
+        <FaSearch />
+      </Link>
+      <input
+        type="text"
+        id="pesquisa"
+        placeholder="Pesquisar:"
+        value={pesquisa}
+        onChange={(e) => setPesquisa(e.target.value)}
+        onKeyDown={handleKeyPress}
+      />
+    </>
+  );
 }
