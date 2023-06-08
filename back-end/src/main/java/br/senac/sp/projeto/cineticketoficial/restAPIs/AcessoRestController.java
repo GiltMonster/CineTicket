d38f2cb9df95eaa03939049b2fa94ca17f22ce4b
GiltMonster <lucassantoslukas2@gmail.com>
2023-06-08 13:57:33 -0300

    Add: CrossOrigin

--- back-end/src/main/java/br/senac/sp/projeto/cineticketoficial/restAPIs/AcessoRestController.java
+++ back-end/src/main/java/br/senac/sp/projeto/cineticketoficial/restAPIs/AcessoRestController.java
@@ -3,6 +3,8 @@ package br.senac.sp.projeto.cineticketoficial.restAPIs;
 import br.senac.sp.projeto.cineticketoficial.entity.Acesso;
 import br.senac.sp.projeto.cineticketoficial.services.AcessoService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/acessos")
 public class AcessoRestController {
     private final AcessoService service;
